551b7f1cfcdcf8ca4b2904317cd8620e2372933f
Eric.R.E <erycrazy@hotmail.com>
2025-11-22 17:25:04 -0300

    Add files via upload

---
+++ Ex0033.py
@@ -0,0 +1,7 @@
+for i in range(0, 50, +2):
+    print(f"Numeros pares são: {i}")
+
+    if i == 48:
+        print("*"*50)
+        for i in range(1, 50, +2):
+            print(f"Os numeros impares são: {i}")
