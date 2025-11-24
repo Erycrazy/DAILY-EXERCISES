72ee7a5fcb8a9fee5ca763369d1acc2be0985dda
Eric.R.E <erycrazy@hotmail.com>
2025-11-24 12:42:51 -0300

    Add files via upload

---
+++ Ex0035.py
@@ -0,0 +1,4 @@
+numero_escolhido = int(input("Digite o numero que quer ver a tabuada: "))
+for i in range(1, 11):
+    resutado = numero_escolhido * i
+    print(f"O numero da tabuada é {numero_escolhido} x {i} = {resutado}")
