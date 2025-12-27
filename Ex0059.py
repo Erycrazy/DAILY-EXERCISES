d1b4bd9c9be2ece0b028d8e86e4e5e09247fa079
Eric.R.E <erycrazy@hotmail.com>
2025-12-27 17:44:12 -0300

    Add files via upload

---
+++ Ex0059.py
@@ -0,0 +1,5 @@
+numero_extenso = ("zero","um", "dois","três","quatro","cinco","seis","sete","oito","nove","dez","onze","doze","treze","quatorze","quinze","dezesseis","dezessete","dezoito","dezenove","vinte")
+
+numero_usuario = int(input("Digite um numero de 0 a 20: "))
+
+print(f"Você digitou o numero: {numero_extenso[numero_usuario]}")
