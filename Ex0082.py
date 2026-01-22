e11b9ac6a491b5034a402269907ffbd7e003f729
Eric.R.E <erycrazy@hotmail.com>
2026-01-22 11:28:08 -0300

    Add files via upload

---
+++ Ex0082.py
@@ -0,0 +1,14 @@
+#Função que adapta ao tamanho da frase
+
+def escreva(fraseParam):
+    print("~" * len(fraseParam)); 
+    print(fraseParam); 
+    print("~" * len(fraseParam)); 
+    
+    
+#Codigo Principal
+
+escreva("Estou!")
+escreva("Estou muito!")
+escreva("Estou muito bonito!")
+escreva("Estou muito bonito hoje!")
