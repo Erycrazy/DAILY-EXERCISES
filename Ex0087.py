ae8bedd08789ddb555c863f9954001054c376e5d
Eric.R.E <erycrazy@hotmail.com>
2026-01-28 18:27:57 -0300

    Add files via upload

---
+++ Ex0087.py
@@ -0,0 +1,9 @@
+#maior e quantidade de varios parametros
+
+def maior(*num):
+    maior_valor = print(f"O maior valor é: {max(num)}"); 
+    quantidade_valores = print(f"Foram informados {len(num)} valores"); 
+    
+    return maior_valor, quantidade_valores; 
+
+maior(5,6,5,4,34,56,7,546,5,56,46,46,4,6,47,); 
